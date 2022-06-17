30cf3a5fc97d30e095b5fb73d85b37ebea90749e
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-06-17 09:45:06 +0300

    adding time zone in settings

--- neighbourhoodwatch/settings.py
+++ neighbourhoodwatch/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hoodwatch',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,13 @@ WSGI_APPLICATION = 'neighbourhoodwatch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hoodwatch',
+        'USER': 'sandra',
+    'PASSWORD':'clubsoda',
     }
 }
+
 
 
 # Password validation
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
